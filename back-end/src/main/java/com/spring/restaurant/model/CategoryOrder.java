e26aba316c518221164b9d8b44cdf670d737f02a
EzzatYaser <azzat.yaser12@gmail.com>
2023-01-14 19:55:39 +0200

    @CreationTimestamp @ Update Timestamp Closes #17

--- back-end/src/main/java/com/spring/restaurant/model/CategoryOrder.java
+++ back-end/src/main/java/com/spring/restaurant/model/CategoryOrder.java
@@ -3,6 +3,10 @@ package com.spring.restaurant.model;
 import jakarta.persistence.*;
 
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
 
 
 @Data // generte getter setter
@@ -12,4 +16,13 @@ import lombok.*;
 public class CategoryOrder extends BaseEntity {
     @Column(name = "name")
     private String name;
+
+
+    @Column(name = "date-create")
+    @CreationTimestamp
+    private Date dateCreate;
+
+    @Column(name = "date-update")
+    @UpdateTimestamp
+    private Date dateUpdate;
 }
