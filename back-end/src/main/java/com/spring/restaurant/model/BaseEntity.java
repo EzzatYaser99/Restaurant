6315386763b7aed7e3fb93f0eeb95b933363d5b2
EzzatYaser <azzat.yaser12@gmail.com>
2023-01-14 16:01:52 +0200

    Lombok Closes #12

--- back-end/src/main/java/com/spring/restaurant/model/BaseEntity.java
+++ back-end/src/main/java/com/spring/restaurant/model/BaseEntity.java
@@ -1,5 +1,13 @@
 package com.spring.restaurant.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // generte getter setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BaseEntity {
     private long id;
 
--- back-end/src/main/java/com/spring/restaurant/model/CategoryOrder.java
+++ back-end/src/main/java/com/spring/restaurant/model/CategoryOrder.java
@@ -1,5 +1,12 @@
 package com.spring.restaurant.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // generte getter setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryOrder extends BaseEntity{
     private String categoryName;
 }
