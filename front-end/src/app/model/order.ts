89f4745a9a119f00635283e62ffda05e729ff379
EzzatYaser <azzat.yaser12@gmail.com>
2023-01-15 18:26:12 +0200

    show images of Orders Closes #33

--- front-end/src/app/model/order.ts
+++ front-end/src/app/model/order.ts
@@ -4,16 +4,16 @@ export class Order {
   date_create:Date;
   date_update:Date;
   description:string;
-  image:string;
+  img:string;
   price:number;
 
-  constructor(id: number, name: string, date_create: Date, date_update: Date, description: string, image: string, price: number) {
+  constructor(id: number, name: string, date_create: Date, date_update: Date, description: string, img: string, price: number) {
     this.id = id;
     this.name = name;
     this.date_create = date_create;
     this.date_update = date_update;
     this.description = description;
-    this.image = image;
+    this.img = img;
     this.price = price;
   }
 }
