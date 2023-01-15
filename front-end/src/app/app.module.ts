209f574fbd2794eaf84ccb5874692068cf9f6b63
EzzatYaser <azzat.yaser12@gmail.com>
2023-01-15 17:13:18 +0200

    Angular Create Order-Items Componant Closes #30

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrderItemsComponent } from './componants/order-items/order-items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderItemsComponent
   ],
   imports: [
     BrowserModule,
