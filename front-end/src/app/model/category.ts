08489e264d172bee145626893a913b9b1c3d3559
EzzatYaser <azzat.yaser12@gmail.com>
2023-01-15 21:47:15 +0200

    handle logos of Category Closes #39

--- front-end/src/app/model/category.ts
+++ front-end/src/app/model/category.ts
@@ -2,12 +2,15 @@ export class Category {
 
   id:number;
   name:string;
+  logo:string;
   date_create:Date;
   date_update:Date;
 
-  constructor(id: number, name: string, date_create: Date, date_update: Date) {
+
+  constructor(id: number, name: string, logo: string, date_create: Date, date_update: Date) {
     this.id = id;
     this.name = name;
+    this.logo = logo;
     this.date_create = date_create;
     this.date_update = date_update;
   }
