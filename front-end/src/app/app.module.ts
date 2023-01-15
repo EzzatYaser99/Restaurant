d7afbd92afb04b24ba6ead0de71d1a85c6e04215
EzzatYaser <azzat.yaser12@gmail.com>
2023-01-15 18:09:21 +0200

    HttpClientModule & @CrossOrigin Closes #32

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OrderItemsComponent } from './componants/order-items/order-items.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -12,9 +13,13 @@ import { OrderItemsComponent } from './componants/order-items/order-items.compon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
