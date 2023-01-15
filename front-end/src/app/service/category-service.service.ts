f439af9b6b17dad8d95b5c3437ac0760ec466349
EzzatYaser <azzat.yaser12@gmail.com>
2023-01-15 20:09:47 +0200

    Angular Create Category Service Closes #37

---
+++ front-end/src/app/service/category-service.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {map, Observable} from "rxjs";
+import {Category} from "../model/category";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryServiceService {
+
+  baseUrl='http://localhost:8080/api/allCategories';
+  constructor( private http:HttpClient) { }
+
+  getAllCategory():Observable<Category[]>{
+    return this.http.get<Category[]>(this.baseUrl).pipe(
+      map(
+        response=>response
+      )
+
+    )
+
+
+  }
+
+
+}
