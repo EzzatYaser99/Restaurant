0d35dcea719bf65ee95e78635a87669906343328
EzzatYaser <azzat.yaser12@gmail.com>
2023-01-17 15:06:27 +0200

    get Order by key Round 1 Closes #59

--- front-end/src/app/componants/serach-order/serach-order.component.ts
+++ front-end/src/app/componants/serach-order/serach-order.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {OrderServiceService} from "../../service/order-service.service";
+import {Order} from "../../model/order";
 
 @Component({
   selector: 'app-serach-order',
   templateUrl: './serach-order.component.html',
   styleUrls: ['./serach-order.component.css']
 })
-export class SerachOrderComponent {
+export class SerachOrderComponent implements OnInit{
 
+
+  orders:Order[]=[]
+  constructor( private orderServive:OrderServiceService) {
+  }
+
+
+  ngOnInit(): void {
+  }
+
+  doSearch(value: string) {
+
+    alert(value);
+  this.orderServive.getOrdersByKey(value).subscribe(
+    data=>{
+      this.orders=data;
+
+    }
+  )
+  }
 }
